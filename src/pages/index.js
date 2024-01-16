813e7e185306e3571bcef6c3159871bcb706ddcc
Үүрцайх Норовсамбуу <you@example.com>
2024-01-15 20:08:19 -0800

    Jan16M

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import { Button, TextField, Card } from "@/components";
 import Link from "next/link";
 import styles from "@/styles/pages/Home.module.css";
+import { Tailwind } from "./tailwindcss";
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Tailwind />
       <h1 className={styles.title}>Home</h1>
       <Link href={"/abc"}>Move To Abc</Link>
       <Button />
---
+++ src/pages/tailwindcss.js
@@ -0,0 +1,15 @@
+export const Tailwind = () => {
+  return (
+    <div className="flex justify-between">
+      <h1 className="text-3xl font-bold underline text-green-700 p-8 border-solid border border-sky-500">
+        Hello world!
+      </h1>
+      <h1 className="text-3xl font-bold underline text-green-700 p-8 border-solid border border-sky-500">
+        Hello world!
+      </h1>
+      <h1 className="text-3xl font-bold underline text-green-700 p-8 border-solid border border-sky-500">
+        Hello world!
+      </h1>
+    </div>
+  );
+};
